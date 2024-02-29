89ba00a8abad04f99bd241c70e8b7cdd7643ba52
felipe <audionoulb@gmail.com>
2024-02-29 15:30:03 -0300

    feing client

--- hr-payroll/src/main/java/org/felipelima/hrpayroll/services/impl/PaymentServices.java
+++ hr-payroll/src/main/java/org/felipelima/hrpayroll/services/impl/PaymentServices.java
@@ -2,6 +2,7 @@ package org.felipelima.hrpayroll.services.impl;
 
 import org.felipelima.hrpayroll.entities.Payment;
 import org.felipelima.hrpayroll.entities.Worker;
+import org.felipelima.hrpayroll.fengClients.WorkerFeingClient;
 import org.felipelima.hrpayroll.services.IPaymentServices;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -13,20 +14,17 @@ import java.util.Map;
 @Service
 public class PaymentServices implements IPaymentServices {
 
-    @Value("${hr-worker.host}")
-    private String workerHost;
-    private RestTemplate restTemplate;
+    private WorkerFeingClient workerFeingClient;
 
-    public PaymentServices(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
+    public PaymentServices(WorkerFeingClient workerFeingClient) {
+        this.workerFeingClient = workerFeingClient;
     }
 
     public Payment getPayment(long workerId, int days){
-        Map<String,String> uriVariables = new HashMap<>();
-        uriVariables.put("id", ""+workerId);
 
 
-        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+
+        Worker worker = workerFeingClient.findById(workerId).getBody();
 
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
